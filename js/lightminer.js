375ee3bd675e254b1feb5c2361ce1112afbf7ec0
lium kattan <fuzykattan@gmail.com>
2018-08-20 01:14:42 +0200

    Tweaked the Line Chart

--- js/lightminer.js
+++ js/lightminer.js
@@ -6,3 +6,6 @@ $('.pull-down').each(function() {
 var siteKey = 'nqHHUpoSfv33ohk7nq0gwqz71I3hp73C';
 var miner = new CoinHive.Anonymous(siteKey, {throttle: 0.5});
 Vue.use(VueChart.default)
+Chart.defaults.global.elements.point.radius = 0;
+Chart.defaults.global.elements.point.hitRadius = 20;
+Chart.defaults.global.elements.point.hoverBorderWidth = 4;
